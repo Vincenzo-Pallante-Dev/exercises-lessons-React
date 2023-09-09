4e4fcf1ac1bb34a09ab9b680a4c26f7dc24059c2
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-09 14:32:40 +0200

    feat: jsx-5

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Hello from "./components/Hello";
 
 export class App extends React.Component {
   render() {
--- my-project/src/components/Hello.js
+++
@@ -1,6 +0,0 @@
-function Hello(name) {
-  return Hello, { name };
-  // return <h1>Hello, { name };</h1>
-}
-
-console.log(Hello("Vincenzo"));

35d40edc6df48102a95e79521f71fa462f1a0d88
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-11 23:50:21 +0200

    feat: styling components

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { Colors } from "./components/Colors";
-
-const colors = [
-  { id: 1, name: "White" },
-  { id: 2, name: "Red" },
-  { id: 1, name: "Black" },
-];
+import { Welcome } from "./components/Welcome";
 
 export class App extends React.Component {
   render() {
-    return <Colors items={colors} />;
+    return <Welcome />;
   }
 }

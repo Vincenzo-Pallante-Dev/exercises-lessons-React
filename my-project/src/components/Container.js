5c5a7acf34b324cc0ffbe7508b9fc723475f6fff
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-15 16:34:59 +0200

    fix: exercise

--- my-project/src/components/Container.js
+++ my-project/src/components/Container.js
@@ -4,9 +4,7 @@ export class Container extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="container-title">{this.props.title}</div>
-        <hr></hr>
-        <div>{this.props.children}</div>
+        <p>I'm a paragraph</p>
       </div>
     );
   }

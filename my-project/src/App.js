bdfc72b62d1baf333411475a54709377d42de5b3
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-18 16:05:51 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -5,6 +5,6 @@ const numbers = [10, 13, 14];
 
 export class App extends React.Component {
   render() {
-    return <Sum numbers={numbers} />;
+    return <Sum />;
   }
 }
--- my-project/src/components/Sum.js
+++ my-project/src/components/Sum.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function Sum({ numbers }) {
+export function Sum({ numbers = [20, 10, 23] }) {
   const sum = numbers.reduce((a, b) => a + b);
   return (
     <div>
